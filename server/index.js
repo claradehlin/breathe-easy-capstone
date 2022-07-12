d5f8adb5a9b5417d9d088d10f9dde5284075a500
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-07-12 10:06:47 -0600

    fixed stuff

--- server/index.js
+++ server/index.js
@@ -10,12 +10,13 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/api/reason', getReason)
 app.post('/api/reason', createReason)
 app.delete('/api/reason/:id', deleteReason)
 
 
-app.get('/html', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/home.html'))
 })
 app.get('/css', (req, res) => {
@@ -24,6 +25,7 @@ app.get('/css', (req, res) => {
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/home.js'))
 })
+
 app.get('/html1', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/distraction.html'))
 })
@@ -34,14 +36,54 @@ app.get('/js1', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/distraction.js'))
 })
 
+app.get('/html2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/about.html'))
+})
+app.get('/js2', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/about.js'))
+})
 
-// app.get('/css', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../styles.css'))
-// })
+app.get('/html3', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/reasons.html'))
+})
+app.get('/js3', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/reasons.js'))
+})
+app.get('/css3', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/reasons.css'))
+})
 
-// app.get('/js', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../index.js'))
-// })
+app.get('/html4', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/skills.html'))
+})
+app.get('/js4', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/skills.js'))
+})
+
+app.get('/html5', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/support.html'))
+})
+app.get('/js5', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/support.js'))
+})
+
+app.get('/html6', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/to-breathe.html'))
+})
+app.get('/js6', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/to-breathe.js'))
+})
+
+app.get('/html7', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/worry-list.html'))
+})
+app.get('/js7', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/worry-list.js'))
+})
+
+app.get('/js8', (req, res) => {
+    res.sendFile(path.join(__dirname, '../server/controller.js'))
+})
 
 const port = process.env.PORT || 4005
 
